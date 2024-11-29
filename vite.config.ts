9c57e67e026b18428087f98ad8b4de563b747016
xbrownx <asbrownn@gmail.com>
2024-11-29 18:28:28 +1100

    desktop

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import react from '@vitejs/plugin-react';import { defineConfig } from 'vite';import viteTsconfigPaths from 'vite-tsconfig-paths';// https://vite.dev/config/export default defineConfig({    base: '.',    plugins: [react(), viteTsconfigPaths()],});
+import react from '@vitejs/plugin-react';import { defineConfig } from 'vite';import viteTsconfigPaths from 'vite-tsconfig-paths';// https://vite.dev/config/export default defineConfig({    base: './',    plugins: [react(), viteTsconfigPaths()],});
